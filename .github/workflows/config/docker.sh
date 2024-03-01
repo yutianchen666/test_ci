8cd0e83dd500ba333b2349ae55272db49c66ce25
yutianchen666 <tianchen.yu@intel.com>
2024-03-01 15:45:26 +0000

    sync to new

--- .github/workflows/config/docker.sh
+++ .github/workflows/config/docker.sh
@@ -5,13 +5,13 @@ build_and_prune() {
     # Set TARGET and DF-SUFFIX using the passed in parameters
     local TARGET="$1"
     local DF_SUFFIX="$2"
-    local PYTHON_V="$3:"
+    local PYTHON_V="$3:-3.9"
     local HTTP_PROXY="$4:"
     local HTTPS_PROXY="$5:"
     
     docker_args=()
     if [ -n "$python_v" ]; then
-        docker_args+=("--build-arg python_v=${{PYTHON_V}}")
+        docker_args+=("--build-arg python_v=${PYTHON_V}")
     fi
     if [ -n "$HTTP_PROXY" ]; then
         docker_args+=("--build-arg http_proxy=${HTTP_PROXY}")
