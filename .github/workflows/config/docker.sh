d4e688be6d252b15823fd6dc1916d2278be5e350
yutianchen666 <tianchen.yu@intel.com>
2024-03-01 15:57:20 +0000

    sync to new

--- .github/workflows/config/docker.sh
+++ .github/workflows/config/docker.sh
@@ -11,7 +11,7 @@ build_and_prune() {
     
     docker_args=()
     if [ -n "$PYTHON_V" ]; then
-        docker_args+=("--python_v PYTHON_V")
+        docker_args+=("--python_v ${PYTHON_V}")
     fi
     if [ -n "$HTTP_PROXY" ]; then
         docker_args+=("--build-arg http_proxy=${HTTP_PROXY}")
